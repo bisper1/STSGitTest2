22091f205c8018c6732a0497413f82a2ab0e1624
bisper1 <bisper@naver.com>
2020-12-30 14:33:40 +0900

    GitTest2.java 2번째 수정.

--- src/GitTest2.java
+++ src/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("STS Git Test");
+		System.out.println("STS Git Test2 : github에서 다운받아 내컴퓨터에 저장하기");
 	}
 }
